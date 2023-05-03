11399a157710641788539be90bb01f573783082c
Mubarak Ahmad <mubarakelmughrabi@gmail.com>
2023-05-03 19:17:03 +0800

    add message

---
+++ tests/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+#include "../include/strfmt/strfmt.hpp"
+int main()
+{
+    // cout << splitString("load file.csv")[0] << endl;
+    // cout << splitString("load,file.csv", ',')[0];
+    // vector<string> words;
+    // words.insert(words.begin(), {" words   ", "a  re", "c   ool  "});
+    // // cout << join(words, ",,,");
+    // cout <<  '.'<< sanitizeStrings(words)[0] << ".";
+    // vector<int> numbers = {4448, 15151554, 545454};
+    // cout << convertToStrings( numbers)[2];
+
+    string command;
+        printf("\x1B[31mTexting\033[0m\t\t");
+}
